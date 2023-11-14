efdbe0ce44a5a2851f08876ac5dfae4630e72c36
rymnd0800 <raymundregalado8@gmail.com>
2023-11-14 17:01:03 +0800

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FoodComponent } from './pages/food/food.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'food', pathMatch: 'full'},
+  {path: 'food', component: FoodComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoodComponent } from './pages/food/food.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FoodSearchComponent } from './components/food-search/food-search.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodComponent,
+    NavComponent,
+    FoodSearchComponent,
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
