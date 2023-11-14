0afd5506bdc346d832b1be6b1a9773882e067009
rymnd0800 <raymundregalado8@gmail.com>
2023-11-14 19:13:24 +0800

    finish main ui

--- src/app/foods.ts
+++ src/app/foods.ts
@@ -5,7 +5,8 @@ export const foods = [
         "description": "Classic Italian pasta dish with savory meat sauce",
         "rating": 4.5,
         "location": "Italian Restaurant",
-        "price": 12.99
+        "price": 12.99,
+        "src" : "https://www.inspiredtaste.net/wp-content/uploads/2023/01/Spaghetti-with-Meat-Sauce-Recipe-Video.jpg"
       },
       {
         "id": 2,
@@ -13,7 +14,8 @@ export const foods = [
         "description": "Tender chicken pieces in a creamy tomato-based curry sauce",
         "rating": 4.8,
         "location": "Indian Restaurant",
-        "price": 14.99
+        "price": 14.99,
+        "src" : "https://www.thecookingcollective.com.au/wp-content/uploads/2021/08/finished-tikka-masala-with-vegetables-and-roti-bread.jpg"
       },
       {
         "id": 3,
@@ -21,7 +23,8 @@ export const foods = [
         "description": "Assorted sushi rolls with fresh fish and rice",
         "rating": 4.7,
         "location": "Japanese Restaurant",
-        "price": 18.50
+        "price": 18.50,
+        "src" : "https://i.redd.it/qrqa6l9zq6f11.jpg"
       },
       {
         "id": 4,
@@ -29,7 +32,8 @@ export const foods = [
         "description": "Crisp romaine lettuce with Caesar dressing, croutons, and parmesan cheese",
         "rating": 4.2,
         "location": "Salad Bar",
-        "price": 9.99
+        "price": 9.99,
+        "src" : "https://www.simplyrecipes.com/thmb/qKL0-KpK_Lq9kmr14Fxl7IcSQMY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Caesar-Salad-METHOD-07-2-87c72fc60da74b38b5d14022d8f9eb01.jpg"
       },
       {
         "id": 5,
@@ -37,7 +41,8 @@ export const foods = [
         "description": "Classic pizza with tomato, mozzarella, and fresh basil",
         "rating": 4.6,
         "location": "Pizzeria",
-        "price": 10.99
+        "price": 10.99,
+        "src" : "https://saltandbaker.com/wp-content/uploads/2023/03/margherita-pizza.jpg"
       },
       {
         "id": 6,
@@ -45,7 +50,8 @@ export const foods = [
         "description": "Juicy beef patty with lettuce, tomato, and special sauce",
         "rating": 4.4,
         "location": "Burger Joint",
-        "price": 13.50
+        "price": 13.50,
+        "src" : "https://www.fiestaspices.com/wp-content/uploads/2016/08/ss-hamduo-20230504.jpg"
       },
       {
         "id": 7,
@@ -53,7 +59,8 @@ export const foods = [
         "description": "Assorted vegetables stir-fried in a savory sauce",
         "rating": 4.3,
         "location": "Vegetarian Cafe",
-        "price": 11.99
+        "price": 11.99,
+        "src" : "https://www.onceuponachef.com/images/2017/02/Asian-Vegetable-Stir-Fry-3.jpg"
       },
       {
         "id": 8,
@@ -61,7 +68,8 @@ export const foods = [
         "description": "Rich chocolate cake with fudge icing",
         "rating": 4.9,
         "location": "Dessert Shop",
-        "price": 8.50
+        "price": 8.50,
+        "src" : "https://juliemarieeats.com/wp-content/uploads/2023/08/Chocolate-Fudge-Cake-1.jpg"
       },
       {
         "id": 9,
@@ -69,7 +77,8 @@ export const foods = [
         "description": "Fresh salmon fillet grilled to perfection",
         "rating": 4.7,
         "location": "Seafood Grill",
-        "price": 16.99
+        "price": 16.99,
+        "src": "https://ux2cms.imgix.net/images/mixed-seafood-grill-with-thai-chilli-dressing.jpg?w=725&auto=compress,format&blend=https://ux2cms.imgix.net/system-images/gray-overlay-large.png?bs=inherit&balph=0&bm=normal"
       },
       {
         "id": 10,
@@ -77,7 +86,8 @@ export const foods = [
         "description": "Vietnamese soup with rice noodles, beef, and aromatic broth",
         "rating": 4.6,
         "location": "Vietnamese Restaurant",
-        "price": 11.50
+        "price": 11.50,
+        "src": "https://www.inspiredtaste.net/wp-content/uploads/2016/06/Vietnamese-Pho-Soup-Recipe-1.jpg"
       }
     ]
 
